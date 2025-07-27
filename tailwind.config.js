2b966ab25417476766659c1185661ccba2fe6da4
KimSeungbum <SB@MacBook-Pro-8.local>
2025-07-27 17:50:29 +0900

    config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // If you use /pages
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // If you ever use /src
+    // Remove above if you know you don't have them!
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
